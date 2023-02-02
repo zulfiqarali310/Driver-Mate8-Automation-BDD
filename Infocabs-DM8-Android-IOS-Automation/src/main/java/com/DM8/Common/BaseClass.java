95b28d2a213cb459d4df8bbe5085d83874aeaf5e
zulfiqarali310 <62157842+zulfiqarali310@users.noreply.github.com>
2023-02-02 16:19:49 +0500

    new pipeline changes

--- Infocabs-DM8-Android-IOS-Automation/src/main/java/com/DM8/Common/BaseClass.java
+++ Infocabs-DM8-Android-IOS-Automation/src/main/java/com/DM8/Common/BaseClass.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Properties;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
@@ -18,9 +21,20 @@ public class BaseClass {
 
 	public static AppiumDriverLocalService service;
 	public static AppiumDriver driver;
+	
+	// borwserstack Properties
+	 public static String userName1 = "zulfiqarali_Enik9H";
+	  public static String accessKey2 = "uGhgqkKUHhCysnhAgZRk";
+	  
+	  // Pipeline properties
+	  public static String userName = System.getenv("BROWSERSTACK_USERNAME");
+	  public static String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+	  public static String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
+	  public static String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
+	  public static String app = System.getenv("BROWSERSTACK_APP_ID");
 
 	static String UserDir = System.getProperty("user.dir");
-	static String ApplicationPath = UserDir + "/Apps/Stage1-DM8.apk";
+	static String ApplicationPath = UserDir + "/Apps/Latest-DM8.apk";
 	static File file = new File(UserDir + "/src/main/resources/config.properties");
 	static FileInputStream fis = null;
 	public static Properties prop = new Properties();
@@ -53,8 +67,29 @@ public class BaseClass {
 
 
 	public static void setAndroidCapabilities() {
+		
+		
+		  DesiredCapabilities caps = new DesiredCapabilities();
+		    caps.setCapability("device", "Samsung Galaxy S8");
+		    caps.setCapability("app", app);
+		    caps.setCapability("browserstack.local", browserstackLocal);
+		    caps.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
 
-		UiAutomator2Options options = new UiAutomator2Options();
+		
+
+	    // use below when you want to run directly on the Browserstack
+		/*
+	    UiAutomator2Options options = new UiAutomator2Options();
+		options.setDeviceName("Google Pixel 3");
+		options.setPlatformName("Android");
+		options.setPlatformVersion("9.0");
+		options.setApp("bs://0076ceee49c3008653233e133cb55329644812f7");
+		options.setCapability("name", "Test1");*/
+		
+	
+		// use below when you want to run it locally
+
+		/*UiAutomator2Options options = new UiAutomator2Options();
 		options.setDeviceName(prop.getProperty("Android_DeviceNames"));
 		options.setPlatformName(prop.getProperty("Device_Type"));
 		options.setUdid(prop.getProperty("Device_UDiD"));
@@ -63,13 +98,18 @@ public class BaseClass {
 		options.setAppPackage(prop.getProperty("App_package"));
 		options.setAppActivity(prop.getProperty("App_Activity"));
 		//options.headless();
+		//options.setCapability("isHeadless", true);
 		// options.setAutoGrantPermissions(true);
-		// options.setCapability("autoAcceptAlerts", "true");
+		// options.setCapability("autoAcceptAlerts", "true");*/
 
 		try {
+			
+			
+			driver = new AndroidDriver(new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), caps); // it is used for pipeline
 
-			driver = new AndroidDriver(service.getUrl(), options);
-			// driver = new AndroidDriver(new URL(prop.getProperty("ServerURL")),options);
+			/*driver = new AndroidDriver(service.getUrl(), options);*/ // this required for local emulator
+			// driver = new AndroidDriver(new URL(prop.getProperty("ServerURL")),options); // this is not required
+			 //driver = new AndroidDriver(new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), options); // this is required for browserstackckbrowser
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(80));
 			//driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(120));
 
